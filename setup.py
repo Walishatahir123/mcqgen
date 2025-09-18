08fca610dbe407a344a95b43f051c64090a36ef2
Walisha <walishatahir00@gmail.com>
2025-09-18 18:59:02 +0500

    File updated

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
 # for installing local package
+from setuptools import find_packages,setup
+
+setup(
+    name="walisha",
+    version="0.0.1",
+    author="Walisha",
+    author_email="walishatahir00@gmail.com",
+    packages=find_packages(),  # finds all packages with __init__.py
+    install_requires=[
+        "openai",
+        "langchain",
+        "streamlit",
+        "python-dotenv",
+        "PyPDF2",
+        "pandas"
+    ],
+    python_requires=">=3.10",
+)
